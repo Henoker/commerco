126a4a9723d9ea74a7ed3b9b414189797adcb3aa
Henoker <hennybany@gmail.com>
2024-02-10 21:38:12 +0300

    feat: navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
-
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-red-400 underline">Hello</h1>
-  
+    <>
+      <div>
+        <Navbar />
+        <Footer />
+      </div>
+    </>
   );
 }
 
